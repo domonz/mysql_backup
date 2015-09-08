78365fa206cbe5fb5259e6d347c3955c652cb931
Domonkos Zsolt <zsdomonko@me.com>
2015-09-08 15:10:20 +0200

    INIT

---
+++ mysql_backup_dump_opt.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+echo "Reading config...." >&2
+source ./config.cfg
+echo "Config for the backup user: $mysql_user" >&2
+echo "Config for the backup password: $mysql_passwd" >&2
+echo "Config for the backup destination_folder: $dest" >&2
+
+timestamp=$(date +"%F")
+
+#mkdir -p $dest/MYSQL_DUMP
+
+#MYSQL DUMP SESSION
+
+#databases=`mysql --user=$mysql_user -p$mysql_passwd -e "SHOW DATABASES;"`
+
+#for db in $databases; do
+  #mysqldump --opt --routines --triggers --events --skip-lock-tables --force --user=$mysql_user --password=$mysql_passwd --databases $db | gzip > "$dest/MYSQL_DUMP/$db-$timestamp.gz"
+#done
+
+#INNODB SESSION
+#mkdir -p $dest/MYSQL_INNOBACKUP
+#innobackupex --stream=tar --user=$mysql_user --password=$mysql_passwd --defaults-file=/etc/mysql/my.cnf ./ | gzip -c -9 > $dest/MYSQL_INNOBACKUP/full.$timestamp.tar.gz
+
+
+while getopts ":di" opt; do
+  case $opt in
+    d)
+      mkdir -p $dest/MYSQL_DUMP
+        databases=`mysql --user=$mysql_user -p$mysql_passwd -e "SHOW DATABASES;"`
+
+			for db in $databases; do
+  				mysqldump --opt --routines --triggers --events --skip-lock-tables --force --user=$mysql_user --password=$mysql_passwd --databases $db | gzip > "$dest/MYSQL_DUMP/$db-$timestamp.gz"
+			done
+	cd $dest/MYSQL_DUMP	 			
+    ;;
+    i)
+     mkdir -p $dest/MYSQL_INNOBACKUP
+	 innobackupex --stream=tar --user=$mysql_user --password=$mysql_passwd --defaults-file=/etc/mysql/my.cnf ./ | gzip -c -9 > $dest/MYSQL_INNOBACKUP/full.$timestamp.tar.gz
+	 cd $dest/MYSQL_INNOBACKUP
+	     ;;   
+     \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
